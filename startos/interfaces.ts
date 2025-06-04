bca326edb97fd8d8ad9b50dc595c128237328ba4
aldum <aldum@artixlinux.org>
2025-06-04 15:05:17 +0200

    style: format interfaces

--- startos/interfaces.ts
+++ startos/interfaces.ts
@@ -2,24 +2,25 @@ import { sdk } from './sdk'
 import { T } from '@start9labs/start-sdk'
 import { uiPort } from './utils'
 
-export const setInterfaces = sdk.setupInterfaces(async ({ effects }: { effects: T.Effects }) => {
-  const uiMulti = sdk.MultiHost.of(effects, 'ui-multi')
-  const uiMultiOrigin = await uiMulti.bindPort(uiPort, {
-    protocol: 'http',
-  })
-  const ui = sdk.createInterface(effects, {
-    name: 'Web UI',
-    id: 'ui',
-    description: 'The web interface',
-    type: 'ui',
-    masked: false,
-    schemeOverride: null,
-    username: null,
-    path: '',
-    query: {},
-  })
+export const setInterfaces = sdk.setupInterfaces(
+  async ({ effects }: { effects: T.Effects }) => {
+    const uiMulti = sdk.MultiHost.of(effects, 'ui-multi')
+    const uiMultiOrigin = await uiMulti.bindPort(uiPort, {
+      protocol: 'http',
+    })
+    const ui = sdk.createInterface(effects, {
+      name: 'Web UI',
+      id: 'ui',
+      description: 'The web interface',
+      type: 'ui',
+      masked: false,
+      schemeOverride: null,
+      username: null,
+      path: '',
+      query: {},
+    })
 
-  const uiReceipt = await uiMultiOrigin.export([ui])
+    const uiReceipt = await uiMultiOrigin.export([ui])
 
-  return [uiReceipt]
-})
+    return [uiReceipt]
+  })
