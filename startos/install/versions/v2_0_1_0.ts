3ef0d4b8947cd89a0d9b480325ff387fcdb3052b
aldum <aldum@artixlinux.org>
2025-10-09 18:44:10 +0200

    feat: bump Immich (2.0.1)

--- startos/install/versions/v2_0_1_0.ts
+++ startos/install/versions/v2_0_1_0.ts
@@ -1,7 +1,7 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
-export const v2_0_0_0 = VersionInfo.of({
-  version: '#noml:2.0.0:0',
+export const v2_0_1_0 = VersionInfo.of({
+  version: '#noml:2.0.1:0',
   releaseNotes: 'Initial release for StartOS',
   migrations: {
     up: async ({ }) => {
