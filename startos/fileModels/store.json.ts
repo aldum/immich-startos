efc2483bab8945d48b2858377d9218dbd77ad5f4
aldum <aldum@artixlinux.org>
2025-10-09 18:44:10 +0200

    feat: add store.json

---
+++ startos/fileModels/store.json.ts
@@ -0,0 +1,32 @@
+import { matches, FileHelper } from '@start9labs/start-sdk'
+
+const { object, string } = matches
+
+const shape = object({
+  admin: object({
+    uuid: string,
+    email: string,
+    pwHash: string
+  }).optional()
+})
+
+export const storeJson = FileHelper.json(
+  {
+    volumeId: 'main',
+    subpath: '/store.json',
+  },
+  shape,
+)
+
+/**
+ * @description Read the store and throw an error if it's not present.
+ * Since it should get created upon installation automatically, this is generally not a possible problem.
+ * @returns storeJson
+ *   */
+export const unsafeReadStore = async () => {
+  const store = await storeJson.read().once()
+  if (!store) {
+    throw new Error("Store is missing!")
+  }
+  return store
+}
