2bc02abd560c6a6d2e19fdcd946d1b57011d3a3e
aldum <aldum@artixlinux.org>
2025-12-31 08:19:06 +0100

    feat: create store on install

--- startos/install/versionGraph.ts
+++ startos/install/versionGraph.ts
@@ -1,9 +1,11 @@
 import { VersionGraph } from '@start9labs/start-sdk'
 import { current, other } from './versions'
+import { storeJson } from '../fileModels/store.json'
 
 export const versionGraph = VersionGraph.of({
   current,
   other,
   preInstall: async (effects) => {
+    storeJson.write(effects, {})
   },
 })
